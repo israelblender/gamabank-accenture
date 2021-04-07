86aa006e21080d17d6f1f2975f054e9a91f4fb31
israelblender <israelgomes.prog@gmail.com>
2021-04-07 03:27:25 -0300

    Implementação de segurança de senhas

--- src/api/services/login.service.js
+++ src/api/services/login.service.js
@@ -1,8 +1,16 @@
 const loginRepository = require("../repositories/login.repository");
+const bcrypt = require('bcrypt');
 
 const checkLogin = async(username, password)=>{
-    const result = await loginRepository.getInformationLogin(username, password)
-    if (result.length > 0) return {isValid: true, cpf:result.cpf}
-    else return {isValid: false}
+    const info_user = await loginRepository.getInformationLogin(username)
+
+    if (info_user.length > 0){
+        const password_equal = await bcrypt.compareSync(password, info_user[0].senha)
+
+        if (password_equal) return {isValid: true, id:info_user[0].id}
+        else return {isValid: false, message: 'Password Incorreto'}
+    }
+    else return {isValid: false, message: 'Username Inesistente'}
+    
 }
-modules.exports = { checkLogin }
+module.exports = { checkLogin }
--- src/configs/env.js
+++ src/configs/env.js
@@ -2,5 +2,7 @@
 
 module.exports = {
     env: process.env.NODE_ENV,
-    secret: process.env.JWT_SECRET || 'Shu7UUp0RsHutD0wN'
+    secret: process.env.JWT_SECRET || 'Shu7UUp0RsHutD0wN',
+    token_exp: process.env.TOKEN_EXPIRATION_TIME || 300,
+    algorithm: process.env.ALGORITHM || 'HS256'
 }
